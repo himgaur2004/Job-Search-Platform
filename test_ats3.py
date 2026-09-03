417339a994ec068c2a6ddc16d19c2b81533fe518
Gaurav <bumiharhimanshusingh@gmail.com>
2026-09-03 09:07:18 +0530

    Delete test_ats3.py

--- test_ats3.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-from services.ats_aggregator import fetch_all_ats
-
-jobs = fetch_all_ats("Software Engineer", "India")
-for j in jobs:
-    print(f"Title: {j['title']} | Location: {j['location']}")
