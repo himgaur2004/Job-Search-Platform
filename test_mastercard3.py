ae57749326a86736d53fe1de7ad296ba17df290c
Gaurav <bumiharhimanshusingh@gmail.com>
2026-09-03 09:06:44 +0530

    Delete test_mastercard3.py

--- test_mastercard3.py
+++
@@ -1,9 +0,0 @@
-import requests
-import socket
-
-domain = "mastercard.wd1.myworkdayjobs.com"
-try:
-    ip = socket.gethostbyname(domain)
-    print(f"{domain} resolves to {ip}")
-except Exception as e:
-    print(e)
